bf2631112621ce0aad7b88a952a3becd46156146
fardeenkhan15 <mdfardeen232002@gmail.com>
2024-07-08 13:03:46 +0530

    fix

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -17,6 +17,7 @@ const MyProfile = () => {
     const fetchPosts = async () => {
       const response = await fetch(`/api/users/${session?.user.id}/posts`);
       const data = await response.json();
+      console.log('profile', data)
 
       setMyPosts(data);
     };
